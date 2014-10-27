d8e6fdcd6d2a3bd825702bd9675f1e67c12fb9aa
Christian Bundy <me@christianbundy.com>
2014-10-27 16:16:48 -0700

    Change URL to Fraction

--- sshcd.rb
+++ sshcd.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Sshcd < GithubGistFormula
-  homepage 'https://github.com/christianbundy/sshcd'
-  url 'https://raw.github.com/christianbundy/sshcd/master/sshcd'
+  homepage 'https://github.com/fraction/sshcd'
+  url 'https://raw.github.com/fraction/sshcd/master/sshcd'
   sha1 'c988d9e9ae6c3ec73e891d9d265f7611e1bf9209'
 end
