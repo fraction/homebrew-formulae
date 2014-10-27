2a3fd3a78f589eb7097edd5881291461c579d064
Christian Bundy <me@christianbundy.com>
2014-10-27 16:17:30 -0700

    Add murder

---
+++ murder.rb
@@ -0,0 +1,7 @@
+require 'formula'
+
+class Murder < GithubGistFormula
+  homepage 'https://github.com/fraction/murder'
+  url 'https://raw.github.com/fraction/murder/master/murder'
+  sha1 '516deae29774160c4ba8bb9f1dabb65ff256dffd'
+end
