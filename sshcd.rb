3bb8c59d8ac35f23793824d1e9b27c649a07aea2
Christian Bundy <me@christianbundy.com>
2014-10-27 13:00:24 -0700

    Add sshcd

---
+++ sshcd.rb
@@ -0,0 +1,7 @@
+require 'formula'
+
+class Sshcd < GithubGistFormula
+  homepage 'https://github.com/christianbundy/sshcd'
+  url 'https://raw.github.com/christianbundy/sshcd/master/sshcd'
+  sha1 'c988d9e9ae6c3ec73e891d9d265f7611e1bf9209'
+end
