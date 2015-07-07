cb1d483d70b075774f7dd15787e3c8137d6fbc4f
Christian Bundy <christianbundy@fraction.io>
2015-07-07 15:17:39 -0700

    Use correct sshcd sha1

--- sshcd.rb
+++ sshcd.rb
@@ -3,5 +3,5 @@ require 'formula'
 class Sshcd < GithubGistFormula
   homepage 'https://github.com/fraction/sshcd'
   url 'https://raw.github.com/fraction/sshcd/master/sshcd'
-  sha1 'c988d9e9ae6c3ec73e891d9d265f7611e1bf9209'
+  sha1 '509b0a62293793a03cb7bf6362e27a0b420bb5c6'
 end
